0cdf622d24a286a35d915ad3952159362960dc38
Wilky2 <wilkyclervil@yahoo.com>
2025-10-11 08:59:06 -0400

    Prepare for deployment

---
+++ part3/phonebook/backend/index.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(express.json())
+
+morgan.token('body', (req) => {
+  if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('dist'))
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateUniqueId = () => {
+  let newId;
+  const existingIds = new Set(persons.map(p => p.id));
+
+  do {
+    newId = Math.floor(Math.random() * 1000000).toString();
+  } while (existingIds.has(newId));
+
+  return newId;
+}
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+      return response.status(400).json({ 
+          error: 'name missing' 
+      })
+    }
+
+    if (!body.number) {
+      return response.status(400).json({ 
+          error: 'number missing' 
+      })
+    }
+
+    const isNameAlreadyExist = persons.some(person=>person.name.toLowerCase() === body.name.toLowerCase())
+
+    if(isNameAlreadyExist){
+      return response.status(400).json({ 
+          error: 'name must be unique'
+      })
+    }
+
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateUniqueId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    person ? response.json(person) : response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+    const html = `
+    <p>Phonebook has info for ${persons.length} ${persons.length > 1 ? 'people' : 'person'}</p>
+    <p>${new Date().toString()}</p>
+    `
+    response.send(html)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
